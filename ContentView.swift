400bc4015bc41b558b670938ab6c9bfe414de497
Paulo Baltieri <baltieri_paulo@hotmail.com>
2023-12-28 19:35:22 -0300

    Create app

---
+++ ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  Scrum_List
+//
+//  Created by Paulo Baltieri on 28/12/23.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ Scrum_ListApp.swift
@@ -0,0 +1,17 @@
+//
+//  Scrum_ListApp.swift
+//  Scrum_List
+//
+//  Created by Paulo Baltieri on 28/12/23.
+//
+
+import SwiftUI
+
+@main
+struct Scrum_ListApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
