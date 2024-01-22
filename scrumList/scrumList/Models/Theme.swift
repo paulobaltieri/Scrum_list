ca1a85c2ce7772251e697eb0fe86d10ab9959795
Paulo Baltieri <baltieri_paulo@hotmail.com>
2024-01-22 20:59:32 -0300

    Ädicionando card ao app

--- scrumList/scrumList/Models/Theme.swift
+++ scrumList/scrumList/Models/Theme.swift
@@ -1,27 +1,28 @@
 import SwiftUI
 
 enum Theme: String {
-     case bubblegum
-     case buttercup
-     case indigo
-     case lavender
-     case magenta
-     case navy
-     case orange
-     case oxblood
-     case periwinkle
-     case poppy
-     case purple
-     case seafoam
-     case sky
-     case tan
-     case teal
-     case yellow
+    
+    case bubblegum
+    case buttercup
+    case indigo
+    case lavender
+    case magenta
+    case navy
+    case orange
+    case oxblood
+    case periwinkle
+    case poppy
+    case purple
+    case seafoam
+    case sky
+    case tan
+    case teal
+    case yellow
     
     var accentColor: Color {
-        switch self{
+        switch self {
         case .bubblegum, .buttercup, .lavender, .orange, .periwinkle, .poppy, .seafoam, .sky, .tan, .teal, .yellow: return .black
-                case .indigo, .magenta, .navy, .oxblood, .purple: return .white
+        case .indigo, .magenta, .navy, .oxblood, .purple: return .white
         }
     }
     var mainColor: Color {
