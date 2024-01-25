671343566a24320f0132d3de846b0318862166bb
Paulo Baltieri <baltieri_paulo@hotmail.com>
2024-01-24 21:41:55 -0300

    Configurando lista

--- scrumList/scrumList/View/CardView.swift
+++ scrumList/scrumList/View/CardView.swift
@@ -1,25 +1,27 @@
 import SwiftUI
 
-
 struct CardView: View {
     let scrum: DailyScrum
     var body: some View {
-        VStack(alignment: .leading) {
+        VStack(alignment: .leading){
             Text(scrum.title)
                 .font(.headline)
+                .accessibilityAddTraits(/*@START_MENU_TOKEN@*/.isHeader/*@END_MENU_TOKEN@*/)
             Spacer()
-            HStack {
+            HStack{
                 Label("\(scrum.attendees.count)", systemImage: "person.3")
+                    .accessibilityLabel("\(scrum.attendees.count) attendees")
                 Spacer()
                 Label("\(scrum.lengthInMinutes)", systemImage: "clock")
-                    .padding(.trailing, 20)
+                    .accessibilityLabel("\(scrum.lengthInMinutes)")
+                    .labelStyle(.trailingIcon)
             }
             .font(.caption)
         }
         .padding()
+        .foregroundColor(scrum.theme.accentColor)
     }
 }
-
 
 struct CardView_Previews: PreviewProvider {
     static var scrum = DailyScrum.sampleData[0]
@@ -29,3 +31,4 @@ struct CardView_Previews: PreviewProvider {
             .previewLayout(.fixed(width: 400, height: 60))
     }
 }
+
